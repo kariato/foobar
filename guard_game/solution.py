ad5f25c9a6eb169991c2a92235d088b2cf9a6925
Mark Davey <kariato@yahoo.com>
2016-02-04 16:33:56 -0500

    Update solution.py

--- guard_game/solution.py
+++ guard_game/solution.py
@@ -1,17 +1,7 @@
-def answer(x):
-    """
-    Adding the digits of a number recursively.
-    """
-    # Set the value for the initial round.
-    number = x
-    # Keep going while there's more than one digit.
+def answer(number):
     while len(str(number)) > 1:
-        # Initialise the round sum to be 0.
-        round_sum = 0
-        # Sum all of the digits of the current number.
-        for i in range(0, len(str(number))):
-            round_sum = round_sum + int(str(number)[i])
-        # Set this round sum as the new number.
-        number = round_sum
-    # Return the result.
+        newNumber = 0
+        for digit  in list(str(number)):
+            newNumber += int(digit)
+        number = newNumber
     return number
